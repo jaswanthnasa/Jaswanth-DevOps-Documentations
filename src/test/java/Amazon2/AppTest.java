87b05fbf33f5fbb7593807d46bf5d33a3c662a50
jaswanth <jas@example.com>
2023-07-19 10:36:13 +0530

    adding files

---
+++ src/test/java/Amazon2/AppTest.java
@@ -0,0 +1,12 @@
+package Amazon2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
