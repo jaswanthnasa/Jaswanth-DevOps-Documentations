5473146c0b1ee846d084e6742ed0a4372c0fd60f
jaswanth <jas@example.com>
2023-10-21 14:07:15 +0530

    organizing documentation foldders

---
+++ shell-scripting/day1/first_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo " welcone to shell script tutorial"
---
+++ shell-scripting/day1/second_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#  create a folder
+mkdir shell_demo
+
+# create 2 files
+cd shell_demo
+touch firstfile secondfile
