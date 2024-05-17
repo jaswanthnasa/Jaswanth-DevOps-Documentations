809795ce4f0f25ca34bba1355184c905bd2f5850
jaswanthnasa <92042814+jaswanthnasa@users.noreply.github.com>
2024-05-17 20:09:46 +0530

    Update main.tf

--- terraform-practice/terraform-workspace/main.tf
+++ terraform-practice/terraform-workspace/main.tf
@@ -8,6 +8,12 @@ resource "aws_instance" "ec2_demo" {
       tags = {
       Name = local.instance_name
     }
+    user_data = "${file("user-data-script.sh")}"
+
+# user_data     = <<-EOF
+        #             #!/bin/bash
+        #             echo "Hello, World!" > hello.txt
+        #           EOF
 
   
 }
